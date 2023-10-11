05f139ecf0a39d9ab0f5d2d6f6d2c0e7e0071a36
buistvo <plus100500@gmail.com>
2023-10-11 22:20:36 +0300

    validate

--- product-service/src/functions/createProduct/handler.ts
+++ product-service/src/functions/createProduct/handler.ts
@@ -6,17 +6,25 @@ import {
 import { middyfy } from '@libs/lambda';
 import { ProductDataService } from '@libs/services/product-data-service';
 import { ProductCreate } from '@libs/types/product';
+import * as yup from 'yup';
+
+const productCreateValidationSchema = yup.object().shape({
+  title: yup.string().required(),
+  price: yup.number().moreThan(0).required(),
+  description: yup.string().nullable(),
+});
 
 const createProduct: ValidatedEventAPIGatewayProxyEvent<ProductCreate> = async (
   event
 ) => {
   try {
+    await productCreateValidationSchema.validate(event.body);
     return formatJSONResponse(
       await new ProductDataService().createProduct(event.body)
     );
   } catch (e) {
-    console.error('Error executing createProduct:', e);
-    return errorResponse(e);
+    console.error('Error executing createProduct:', e.errors || e);
+    return errorResponse(e.errors || e);
   }
 };
 
--- product-service/src/functions/getAvailableProductsList/handler.ts
+++ product-service/src/functions/getAvailableProductsList/handler.ts
@@ -3,18 +3,15 @@ import { errorResponse, formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import { ProductDataService } from '@libs/services/product-data-service';
 
-//import schema from './schema';
-
-const getAvailableProductsList = //: ValidatedEventAPIGatewayProxyEvent<typeof schema> =
-  async (event) => {
-    try {
-      return formatJSONResponse(
-        await new ProductDataService().getAvailableProductsList()
-      );
-    } catch (e) {
-      console.error('Error executing getAvailableProductsList:', e);
-      return errorResponse(e);
-    }
-  };
+const getAvailableProductsList = async (event) => {
+  try {
+    return formatJSONResponse(
+      await new ProductDataService().getAvailableProductsList()
+    );
+  } catch (e) {
+    console.error('Error executing getAvailableProductsList:', e);
+    return errorResponse(e);
+  }
+};
 
 export const main = middyfy(getAvailableProductsList);
