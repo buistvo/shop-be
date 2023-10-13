36ccf79abbab28744f30b39ee3f3e8ba5d814859
buistvo <plus100500@gmail.com>
2023-10-13 12:46:01 +0300

    use cors with middi

--- product-service/src/libs/api-gateway.ts
+++ product-service/src/libs/api-gateway.ts
@@ -12,16 +12,9 @@ export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
   APIGatewayProxyResult
 >;
 
-const headers = {
-  'Access-Control-Allow-Origin': 'https://d16pd4ocp5ok6l.cloudfront.net',
-  'Access-Control-Allow-Methods': 'PUT, POST, GET, OPTIONS',
-  'Access-Control-Allow-Credentials': true,
-};
-
 export const formatJSONResponse = (response: unknown | Array<unknown>) => {
   return {
     statusCode: 200,
-    headers,
     body: JSON.stringify(response),
   };
 };
@@ -29,7 +22,6 @@ export const formatJSONResponse = (response: unknown | Array<unknown>) => {
 export const errorResponse = (message: string, errorCode = 500) => {
   return {
     statusCode: errorCode,
-    headers,
     body: JSON.stringify({ message }),
   };
 };
